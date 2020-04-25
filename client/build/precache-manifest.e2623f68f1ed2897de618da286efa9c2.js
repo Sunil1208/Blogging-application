42c166e68a0e5202756d01896b776523e7d2a043
Sunil1208 <skmy03@gmail.com>
2020-04-25 21:27:04 +0530

    Build Mode

---
+++ client/build/precache-manifest.e2623f68f1ed2897de618da286efa9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940c1e606a75d0b9b6de59584dc7c20a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c13fca6e397e231fe154",
+    "url": "/static/css/main.f5d51f30.chunk.css"
+  },
+  {
+    "revision": "ed5b2a6c6d25e38f1e4d",
+    "url": "/static/js/2.3ab52c7f.chunk.js"
+  },
+  {
+    "revision": "c13fca6e397e231fe154",
+    "url": "/static/js/main.0b9a0bb7.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "810f706cae4a8901415f08590d5e4277",
+    "url": "/static/media/node.810f706c.png"
+  }
+]);
